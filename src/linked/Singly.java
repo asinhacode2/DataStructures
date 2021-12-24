7fc41683a579c4cde950dd6a635db9ebe37cd4c2
sinhatransformed <sinhatranformed@gmail.com>
2021-12-24 14:56:09 -0500

    adding helper methods in singly

--- src/linked/Singly.java
+++ src/linked/Singly.java
@@ -2,18 +2,64 @@ package linked;
 
 public class Singly<T> {
 
+    // having a head and tail to start with
     private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
 
+    // constructor
     public Singly(){
-        this.head = null;
+
     }
 
+    // get size
+    public int getSize(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    // increment and decrement the size
+    public void increment(){
+        this.size++;
+    }
+
+    public void decrement(){
+        this.size--;
+    }
+
+    // return the first
+    public T getFirst(){
+        if(isEmpty()){
+            return null;
+        }
+
+        return this.head.getData();
+    }
+
+    // return the last
+    public T getLast(){
+        if(isEmpty()){
+            return null;
+        }
+
+        return this.tail.getData();
+    }
+
+    // insert at the front
     public void insertAtFront(T data){
-        Node<T> new_node = new Node<>(data, null);
-        new_node.setNext(this.head);
-        this.head = new_node;
+        this.head = new Node<>(data, head);
+
+        if (isEmpty()){
+            tail = head;
+        }
+        increment();
+
     }
 
+    // display
     public void display(){
         Node<T> temp = this.head;
         while(temp != null){
